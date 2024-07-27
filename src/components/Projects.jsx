f1368bc34a73f0774700e83ed4b6669797cc0cda
Vaibtan <vaibhav21296@iiitd.ac.in>
2024-07-27 20:11:53 +0530

    another_commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -42,6 +42,13 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
           <h3 className = 'text-white font-bold text-[25px]'>{name}</h3>
           <p className = 'mt-2 text-secondary font-bold text-[19px]'>{description}</p>
         </div>
+        <div className = 'mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p key = {`tag.name`} className = {`text-[18px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
       </Tilt>
     </motion.div>
   )
