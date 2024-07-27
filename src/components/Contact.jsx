12698a0a5b63829f9443c757fb3f60c829e26b01
Vaibtan <vaibhav21296@iiitd.ac.in>
2024-07-27 22:58:09 +0530

    final_structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,11 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+// 
+//
+//
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -17,9 +22,41 @@ const Contact = () => {
   });
 
   const [loading, setLoading] = useState(false);
-  const handleSubmit = (e) => {}
-  const handleChange = (e) => {} 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value })
+  } 
   
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs.send(
+      'service_x4vvdvv', 
+      'template_zi2q60e',
+      {
+        from_name: form.name,
+        to_name: 'Vaibhav Tanwar',
+        from_email: form.email,
+        to_email: 'vaibhav21296@iiitd.ac.in',
+        message: form.message,
+      },
+      'wdPCCQqUF9qvDpsCb'
+    ).then(() => {
+      setLoading(false);
+      alert('Thank You. I will get back to you ASAP.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false);
+      console.log(error);
+      alert('Something went wrong.')
+    })
+  }
+
   return (
     <div className = 'xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow:hidden'> 
       <motion.div 
